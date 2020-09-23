e4ddf6946ae89c033e8020d64981f61ce2543e39
Miroslav Jackanin <miroslav.jackanin@akademiasovy.sk>
2020-09-23 08:09:04 +0200

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import * as express from 'express';
+import * as fs from "fs";
+import * as menu from "./menu.json";
+import * as articles from "./articles.json";
 
-const app = express(),
+const app = express();
+const router = express.Router();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(router);
 
-app.get("/", function(_req, res) {
-  res.render("index");
+app.use(router); app.use(function(_req, res) {
+  res.status(404).render("404"); });
+
+router.get("/:page", function(req, res) {
+  if (fs.existsSync("views/pages/" + req.params.page + ".ejs")) {
+    res.render("index", { page: req.params.page, menu: menu, articles: articles });
+  }
+  else {
+    res.status(404).render("404", {menu: menu});
+  }
+});
+
+router.get("/", function (req, res) {
+  res.render("index", { page: "main", menu, articles });
 });
 
 app.listen(8080, () => console.log('listening on port 8080'));
